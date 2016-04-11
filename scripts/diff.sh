166afca55c3473bf1df2072ec91549fcd41a3c59
Rufus Pollock <rufus.pollock@okfn.org>
2016-04-11 19:15:32 +0100

    [diff][s]: chop out meta and "header" info from agreements before diffing (23-oct vs 4 dec only).

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -7,12 +7,15 @@
 SCRIPTS=scripts
 AGREEMENT=agreement
 DIFF=diff
+TMPFILE1=tmp1.md
+TMPFILE2=tmp2.md
 
 # 2015-10-23 vs 2015-12-04
-diff -U99999                                    \
-    $AGREEMENT/23-oct-2015/index.md             \
-    $AGREEMENT/4-dec-2015/index.md              \
-        | python $SCRIPTS/diff2html.py          \
+tail -n +18 $AGREEMENT/23-oct-2015/index.md > $TMPFILE1 
+tail -n +15 $AGREEMENT/4-dec-2015/index.md > $TMPFILE2
+
+diff -U99999 $TMPFILE1 $TMPFILE2               \
+        | python $SCRIPTS/diff2html.py         \
         -a 1 > $DIFF/23-oct-vs-4-dec.html
 
 # 2015-12-04 vs 2015-12-09
@@ -28,3 +31,7 @@ diff -U99999                                    \
     $AGREEMENT/index.md              \
         | python $SCRIPTS/diff2html.py          \
         -a 1 > $DIFF/9-dec-vs-12-dec.html
+
+rm $TMPFILE1
+rm $TMPFILE2
+
